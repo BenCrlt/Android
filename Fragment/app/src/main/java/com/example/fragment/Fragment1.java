fce20d019566a778520061f6428a3bdb5f29f643
BenCrlt <benoitcournault@gmail.com>
2020-12-17 16:35:32 +0100

    Première approche fragment sans JetPack

---
+++ Fragment/app/src/main/java/com/example/fragment/Fragment1.java
@@ -0,0 +1,85 @@
+package com.example.fragment;
+
+import android.content.Context;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Fragment1 extends Fragment {
+
+    private final String TAG = "HELLO";
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        Log.d(TAG, "On CreateView " + getClass().getSimpleName());
+        return inflater.inflate(R.layout.fragment_1, container, false);
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "On Create " + getClass().getSimpleName());
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(TAG, "On Start " + getClass().getSimpleName());
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "On Resume " + getClass().getSimpleName());
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(TAG, "On Pause " + getClass().getSimpleName());
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(TAG, "On Stop " + getClass().getSimpleName());
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "On Destroy " + getClass().getSimpleName());
+        super.onDestroy();
+    }
+
+    @Override
+    public void onAttach(@NonNull Context context) {
+        Log.d(TAG, "On Attach " + getClass().getSimpleName());
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        Log.d(TAG, "On ActivityCreated " + getClass().getSimpleName());
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onDestroyView() {
+        Log.d(TAG, "On DestroyView " + getClass().getSimpleName());
+        super.onDestroyView();
+    }
+
+    @Override
+    public void onDetach() {
+        Log.d(TAG, "On Detach " + getClass().getSimpleName());
+        super.onDetach();
+    }
+}
---
+++ Fragment/app/src/main/java/com/example/fragment/MainActivity.java
@@ -0,0 +1,80 @@
+package com.example.fragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final String TAG = "HELLO";
+
+    FragmentManager fm  = getSupportFragmentManager();
+    FragmentA fragmentA;
+    FragmentB fragmentB;
+    FragmentC fragmentC;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.d(TAG, "On Create " + getClass().getSimpleName());
+        setContentView(R.layout.activity_main);
+
+        this.fragmentA = new FragmentA();
+        this.fragmentB = new FragmentB();
+        this.fragmentC = new FragmentC();
+    }
+
+    public void OnClickFragmentA(View v) {
+        if (fm.findFragmentById(R.id.fragmentcontainer) != fragmentA) {
+            fm.popBackStackImmediate();
+            fm.beginTransaction().replace(R.id.fragmentcontainer, fragmentA).addToBackStack(null).commit();
+        }
+    }
+
+    public void onClickFragmentB(View v) {
+        if (fm.findFragmentById(R.id.fragmentcontainer) != fragmentB) {
+            fm.popBackStackImmediate();
+            fm.beginTransaction().replace(R.id.fragmentcontainer, fragmentB).addToBackStack(null).commit();
+        }
+    }
+
+    public void onClickFragmentC(View v) {
+        if (fm.findFragmentById(R.id.fragmentcontainer) != fragmentC) {
+            fm.popBackStackImmediate();
+            fm.beginTransaction().replace(R.id.fragmentcontainer, fragmentC).addToBackStack(null).commit();
+        }
+    }
+
+    @Override
+    public void onStart() {
+        Log.d(TAG, "On Start " + getClass().getSimpleName());
+        super.onStart();
+    }
+
+    @Override
+    public void onResume() {
+        Log.d(TAG, "On Resume " + getClass().getSimpleName());
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        Log.d(TAG, "On Pause " + getClass().getSimpleName());
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        Log.d(TAG, "On Stop " + getClass().getSimpleName());
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroy() {
+        Log.d(TAG, "On Destroy " + getClass().getSimpleName());
+        super.onDestroy();
+    }
+}
